2d0cafaa52cbd9bca5efc96abdadc5f2a38753ab
fabriciogallo97 <150983057+fabriciogallo97@users.noreply.github.com>
2023-11-19 16:53:14 -0600

    update ventas 2

---
+++ API/API/Api/Controllers/VentaController.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APIGestion.Data;
+using Api.Modelo;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("Venta")]
+    public class VentaController : Controller
+    {
+        private readonly Context _context;
+
+        public VentaController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Venta/listaVentas
+        [HttpGet]
+        [Route("listaVentas")]
+        public async Task<ActionResult<IEnumerable<Venta>>> GetVentas()
+        {
+            var ventas = await _context.Venta.ToListAsync();
+            return Ok(ventas);
+        }
+
+        // GET: Venta/BuscarVenta/5
+        [HttpGet]
+        [Route("BuscarVenta/{id}")]
+        public async Task<ActionResult<Venta>> GetVenta(int id)
+        {
+            var venta = await _context.Venta.FindAsync(id);
+
+            if (venta == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(venta);
+        }
+
+
+        [HttpGet]
+        [Route("BuscarVentaPorCodFactura/{codFactura}")]
+        public IActionResult GetVentaPorCodFactura(int codFactura)
+        {
+            var venta = _context.Venta.FirstOrDefault(v => v.CodFactura == codFactura);
+
+            if (venta == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(venta);
+        }
+
+        // POST: Venta/CrearVenta
+        [HttpPost]
+        [Route("CrearVenta")]
+        public async Task<ActionResult<Venta>> CreateVenta(Venta venta)
+        {
+            _context.Venta.Add(venta);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetVenta), new { id = venta.CodFactura }, venta);
+        }
+
+        // PUT: Venta/EditarVenta
+        [HttpPut]
+        [Route("EditarVenta")]
+        public async Task<IActionResult> UpdateVenta(Venta venta)
+        {
+            _context.Entry(venta).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VentaExists(venta.CodFactura))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: Venta/BorrarVenta/5
+        [HttpDelete]
+        [Route("BorrarVenta/{id}")]
+        public async Task<IActionResult> DeleteVenta(int id)
+        {
+            var venta = await _context.Venta.FindAsync(id);
+            if (venta == null)
+            {
+                return NotFound();
+            }
+
+            _context.Venta.Remove(venta);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool VentaExists(int id)
+        {
+            return _context.Venta.Any(v => v.CodFactura == id);
+        }
+    }
+}
--- js/venta.js
+++ js/venta.js
@@ -2,6 +2,82 @@ var contenidoTablaResultado = document.querySelector("#resultados");
 
 var modalCrearVenta = new bootstrap.Modal(document.getElementById('modalCrearVenta'));
 
+const input = document.getElementById("inputCodFactura");
+
+function cargarVentas() {
+    fetch("https://localhost:7203/Venta/listaVentas", {
+      method: "GET",
+    })
+      .then((ventas) => ventas.json())
+      .then((jsonVentas) => {
+        contenidoTablaResultado.innerHTML = ``;
+        console.log(jsonVentas);
+        for (const venta of jsonVentas) {
+          contenidoTablaResultado.innerHTML += `
+            <tr class="table-primary">
+              <td>${venta.codFactura}</td>
+              <td>${venta.nombre}</td>
+              <td>${venta.cedula}</td>
+              <td>${venta.subTotal}</td>
+              <td>${venta.iva}</td>
+              <td>${venta.montoTotal}</td>
+              <td>${venta.fechaVenta}</td>
+              <td>
+                <a name="" id="" class="btn btn-info" onclick="editar(${venta.codFactura},'${venta.nombre}', '${venta.cedula}', ${venta.subTotal}, ${venta.iva}, ${venta.montoTotal}, '${venta.fechaVenta}')" role="button">Editar</a>
+                <a name="" id="" class="btn btn-danger" onclick="eliminar(${venta.codFactura})" role="button">Eliminar</a>
+              </td>
+            </tr>`;
+        }
+      })
+      .catch((error) => {
+        // Aquí puedes manejar los errores de la solicitud
+        console.error(error);
+      });
+  }
+  
+  cargarVentas();
+  
+  // Agrega un listener de eventos al input
+input.addEventListener("input", function () {
+    if (input.value != "") {
+      fetch("https://localhost:7203/Venta/BuscarVenta/" + input.value, {
+        method: "GET",
+      })
+        .then((response) => response.json())
+        .then((venta) => {
+          contenidoTablaResultado.innerHTML = ``;
+          console.log(venta);
+          if (venta !== null) {
+            contenidoTablaResultado.innerHTML += `
+              <tr class="table-primary">
+                <td>${venta.codFactura}</td>
+                <td>${venta.nombre}</td>
+                <td>${venta.cedula}</td>
+                <td>${venta.subTotal}</td>
+                <td>${venta.iva}</td>
+                <td>${venta.montoTotal}</td>
+                <td>${venta.fechaVenta}</td>
+                <td>
+                  <a name="" id="" class="btn btn-info" onclick="editar(${venta.codFactura},'${venta.nombre}', '${venta.cedula}', ${venta.subTotal}, ${venta.iva}, ${venta.montoTotal}, '${venta.fechaVenta}')" role="button">Editar</a>
+                  <a name="" id="" class="btn btn-danger" onclick="eliminar(${venta.codFactura})" role="button">Eliminar</a>
+                </td>
+              </tr>`;
+          } else {
+            // La venta no se encontró
+            contenidoTablaResultado.innerHTML = "Venta no encontrada";
+          }
+        })
+        .catch((error) => {
+          // Aquí puedes manejar los errores de la solicitud
+          console.error(error);
+        });
+    } else {
+      cargarVentas();
+    }
+  });
+  
+
+
 function mostrarModalCrearVenta() {
   modalCrearVenta.show();
 }
